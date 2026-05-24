1b2b03d968ba4474e60e660a75ee7743168efa6d
sperrin <sperrin@f2r7s9.paris.42.school>
2026-05-24 13:30:54 +0200

    ex0

---
+++ ex0/ft_garden_intro.py
@@ -0,0 +1,11 @@
+# File: ft_garden_intro.py
+# Description: Program displaying the name, size and age of a plant.
+# Author: Chichu
+
+if __name__ == "__main__":
+    name = "Rose"
+    height = 25
+    age = 30
+    print("=== Welcome to My Garden ===")
+    print(f"Plant: {name.capitalize()}\nHeight: {height}cm\nAge: {age} days\n")
+    print("=== End of Program ===")

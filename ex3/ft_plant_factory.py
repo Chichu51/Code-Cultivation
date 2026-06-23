f8a0ddca721240bae4350d000e4ff1fdc7f8f61e
sperrin <sperrin@f1r9s2.paris.42.school>
2026-06-23 18:25:57 +0200

    ex3

---
+++ ex3/ft_plant_factory.py
@@ -0,0 +1,34 @@
+# File: ft_plant_growth.py
+# Description: creation and print 5 plants
+# Author: Chichu
+
+class Plant:
+    def __init__(self, name: str, height: float, age: int) -> None:
+        self.name = name
+        self.height = height
+        self.age = age
+
+    def show(self) -> None:
+        print(
+            f"{(self.name.capitalize())}: "
+            f"{self.height}cm, {self.age} days old"
+        )
+
+
+if __name__ == "__main__":
+    print("=== Plant Factory Output ===")
+    plant1 = Plant("rose", 25.0, 30)
+    plant2 = Plant("oak", 200.0, 365)
+    plant3 = Plant("cactus", 5.0, 90)
+    plant4 = Plant("sunflower", 80.0, 45)
+    plant5 = Plant("fern", 15.0, 120)
+    print("Created: ", end="")
+    plant1.show()
+    print("Created: ", end="")
+    plant2.show()
+    print("Created: ", end="")
+    plant3.show()
+    print("Created: ", end="")
+    plant4.show()
+    print("Created: ", end="")
+    plant5.show()

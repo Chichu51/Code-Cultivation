61d049e809b5d10050dbdab0288dbdda2f0aef51
sperrin <sperrin@f1r9s2.paris.42.school>
2026-06-23 16:02:35 +0200

    ex2

--- ex1/ft_garden_data.py
+++ ex1/ft_garden_data.py
@@ -18,8 +18,4 @@ class Plant:
 if __name__ == "__main__":
     print("=== Garden Plant Registry ===")
     plant1 = Plant("rose", 25, 30)
-    plant2 = Plant("sunflower", 80, 45)
-    plant3 = Plant("cactus", 15, 120)
     plant1.show()
-    plant2.show()
-    plant3.show()
--- ex2/ft_plant_growth.py
+++ ex2/ft_plant_growth.py
@@ -1,3 +1,37 @@
 # File: ft_plant_growth.py
-# Description: Definition of a Plant class to store its name, height, and age.
+# Description: simulation plant growth over time
 # Author: Chichu
+
+class Plant:
+    def __init__(self, name: str, height: float, age: int) -> None:
+        self.name = name
+        self.height = height
+        self.age = age
+
+    def grow(self) -> None:
+        self.height += 0.8
+
+    def old(self) -> None:
+        self.age += 1
+
+    def show(self) -> None:
+        print(
+            f"{self.name.capitalize()}: "
+            f"{round(self.height, 1)}cm, {self.age} days old"
+        )
+
+
+if __name__ == "__main__":
+    day = 1
+    print("=== Garden Plant Growth ===")
+    plant1 = Plant("rose", 25.0, 30)
+    growth_total = plant1.height
+    plant1.show()
+    while day <= 7:
+        print(f"=== Day {day} ===")
+        plant1.grow()
+        plant1.old()
+        plant1.show()
+        day += 1
+    growth_total = plant1.height - growth_total
+    print(f"Growth this week: {round(growth_total, 1)}cm")
